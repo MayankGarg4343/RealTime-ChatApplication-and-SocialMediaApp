341a4a899f3880a08e013105ee524902f857fd00
MayankGarg4343 <mgsm4343@gmail.com>
2025-09-19 11:55:57 +0530

    add the auth routes

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from "express"; // it is our web framework and it allows us to make apis easily and quickly. 
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.route.js" // .js is mendatory because we have the defined the type in the package.json is module.
+
+dotenv.config(); // we have to eable this method to read the properties.
+
+const app = express()
+const PORT = process.env.PORT;
+
+
+app.use("/api/auth",authRoutes)
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port: ${PORT}`);
+}) 
