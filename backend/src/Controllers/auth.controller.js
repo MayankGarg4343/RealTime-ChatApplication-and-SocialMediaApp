78421988606b248fddc182ffe44e655cd9ec5188
MayankGarg4343 <mgsm4343@gmail.com>
2025-09-19 19:07:20 +0530

    database connected

--- backend/src/Controllers/auth.controller.js
+++ backend/src/Controllers/auth.controller.js
@@ -4,6 +4,6 @@ export async function signup(req,res){
 export async function login(req,res){
     res.send("login route");
 }
-export async function logout(req,res){
+export function logout(req,res){
     res.send("logout route");
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express"; // it is our web framework and it allows us to ma
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js" // .js is mendatory because we have the defined the type in the package.json is module.
+import { connectDB } from "./lib/db.js";
 
 dotenv.config(); // we have to eable this method to read the properties.
 
@@ -9,8 +10,9 @@ const app = express()
 const PORT = process.env.PORT;
 
 
-app.use("/api/auth",authRoutes)
+app.use("/api/auth",authRoutes);
 
 app.listen(PORT,()=>{
     console.log(`server is running on port: ${PORT}`);
-}) 
+    connectDB();
+})  
