b95ffb9c761cb508f64ce2c736bfad9e98506d67
MayankGarg4343 <mgsm4343@gmail.com>
2025-09-24 21:12:05 +0530

    signup functionality is completed

--- backend/src/Controllers/auth.controller.js
+++ backend/src/Controllers/auth.controller.js
@@ -88,6 +88,8 @@ export async function login(req,res){
         res.status(500).json({message:"Internal Server Error"})
     }
 }
+
 export function logout(req,res){
-    res.send("logout route");
+    res.clearCookie("jwt");
+    res.status(200).json({success: true, message: "logout successful"});
 }
