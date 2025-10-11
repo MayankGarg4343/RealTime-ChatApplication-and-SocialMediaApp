c212f66167dcbb0a16dac290cf27bdb1fb5f7bde
Mayank Garg <151990706+MayankGarg4343@users.noreply.github.com>
2025-10-11 23:18:03 +0530

    Update auth.route.js

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import { signup, login, logout } from "../Controllers/auth.controller.js";
 const router = express.Router()
 
+// creted the routes of the signup, login and logout.
 router.post("/signup", signup);
 router.post("/login", login); 
 router.post("/logout",logout);
